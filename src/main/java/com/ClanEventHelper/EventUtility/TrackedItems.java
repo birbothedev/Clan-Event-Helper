4c65a25e2210a52069077c1030bbf5de47c897ff
birbothedev <123966430+birbothedev@users.noreply.github.com>
2025-03-13 16:03:45 -0400

    Update TrackedItems.java

--- src/main/java/com/ClanEventHelper/EventUtility/TrackedItems.java
+++ src/main/java/com/ClanEventHelper/EventUtility/TrackedItems.java
@@ -1,30 +1,30 @@
-package com.ClanEventHelper.EventUtility;
-
-import net.runelite.api.Item;
-import net.runelite.api.ItemComposition;
-import net.runelite.client.game.ItemManager;
-
-import javax.inject.Inject;
-import java.util.HashSet;
-import java.util.Set;
-
-public class TrackedItems {
-    private static final Set<Integer> TRACKED_ITEM_IDS = new HashSet<>();
-    private static final Set<String> TRACKED_ITEM_NAMES = new HashSet<>();
-
-    @Inject
-    private ItemManager itemManager;
-
-    public TrackedItems(ItemManager itemManager){
-        this.itemManager = itemManager;
-    }
-
-    public TrackedItems(Item item){
-        int itemId = item.getId();
-        String name = "Bones";
-
-        if (TRACKED_ITEM_NAMES.contains(name)){
-
-        }
-    }
-}
+//package com.ClanEventHelper.EventUtility;
+//
+//import net.runelite.api.Item;
+//import net.runelite.api.ItemComposition;
+//import net.runelite.client.game.ItemManager;
+//
+//import javax.inject.Inject;
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//public class TrackedItems {
+//    private static final Set<Integer> TRACKED_ITEM_IDS = new HashSet<>();
+//    private static final Set<String> TRACKED_ITEM_NAMES = new HashSet<>();
+//
+//    @Inject
+//    private ItemManager itemManager;
+//
+//    public TrackedItems(ItemManager itemManager){
+//        this.itemManager = itemManager;
+//    }
+//
+//    public TrackedItems(Item item){
+//        int itemId = item.getId();
+//        String name = "Bones";
+//
+//        if (TRACKED_ITEM_NAMES.contains(name)){
+//
+//        }
+//    }
+//}
